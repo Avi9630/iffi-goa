3be99e9ad6141ef132a824ba46590aff2e6b32fc
Avinash kumar <avinashmca94@gmail.com>
2024-11-01 18:43:35 +0530

    changes

--- resources/views/master-class/nov21.blade.php
+++ resources/views/master-class/nov21.blade.php
@@ -1,37 +1,124 @@
 <div class="mt-4 mb-4">
-    <h2 class="pb-3">November 20th, 2024</h2>
-    {{-- <div class="d-flex tab-content-box"> --}}
+    <h2 class="pb-3">November 21st, 2024</h2>
     <div class="row">
+
         <div class="col-md-2 col-12">
             <div class="master-time">
-                <h4 class="time-div">02:00pm</h4>
-                <h5 class="time-div">05:00pm</h5>
+                <h4 class="time-div">11:00 AM</h4>
+                <h5 class="time-div">12:00 PM</h5>
             </div>
         </div>
+
         <div class="col-md-7 col-12">
             <div class="callout">
-                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab">Beyond The Lens: Funding and
-                    Monetization in Documentary Filmmaking</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s.</p>
-                {{-- <div><a href="#" class="btn cus-prim-btn">Speaker</a></div> --}}
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov21-0-trigger">
+                    Women Safety and Cinema
+                </h4>
+                <p>
+                    The world of cinema should not be a distant entity when it comes to react and express itself with a
+                    conscience. It examines the evolution of female characters, the depiction of violence against women,
+                    and the impact of films on societal attitudes towards women's safety. Also, Implementation of
+                    policies and practices that promote a safe and inclusive workspace environment for women will be
+                    discussed by this panel.
+                </p>
             </div>
         </div>
+
         <div class="col-md-3 col-12">
             <div class="user-profile-icon">
                 <ul>
-                    <li><img src="{{ asset('public/images/master-class/svetlana-naudiyal.jpg') }}" alt="image"
+                    <li><img src="{{ asset('public/images/master-class/Imtiaz_Ali.avif') }}" alt="image"
                             class="img-fluid">
                     </li>
-                    <li><img src="{{ asset('public/images/master-class/svetlana-naudiyal.jpg') }}" alt="image"
+                    <li><img src="{{ asset('public/images/master-class/Kushboo Sundar.jpg') }}" alt="image"
                             class="img-fluid">
                     </li>
-                    <li><img src="{{ asset('public/images/master-class/svetlana-naudiyal.jpg') }}" alt="image"
+                    <li><img src="{{ asset('public/images/master-class/Suhasini Manirathnam.jfif') }}" alt="image"
                             class="img-fluid">
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-    {{-- </div> --}}
+</div>
+
+<div class="mt-4 mb-4">
+    <h2 class="pb-3">November 21st, 2024</h2>
+    <div class="row">
+
+        <div class="col-md-2 col-12">
+            <div class="master-time">
+                <h4 class="time-div">02:30 PM</h4>
+                <h5 class="time-div">04:00 PM</h5>
+            </div>
+        </div>
+
+        <div class="col-md-7 col-12">
+            <div class="callout">
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov21-1-trigger">
+                    360° Cinema:Film Festival Directors' Round Table
+                </h4>
+                <p>
+                    While it comes to showcase meaningful cinema and also to create a future for it, it is pertinent for
+                    the Film Festival Directors to sit & talk on a round table! This roundtable discussion brings
+                    together legendary festival directors to explore the vital role of film festivals in promoting
+                    global cinema.
+                </p>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-12">
+            <div class="user-profile-icon">
+                <ul>
+                    <li><img src="{{ asset('public/images/master-class/Imtiaz_Ali.avif') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                    <li><img src="{{ asset('public/images/master-class/Kushboo Sundar.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="mt-4 mb-4">
+    <h2 class="pb-3">November 21st, 2024</h2>
+    <div class="row">
+
+        <div class="col-md-2 col-12">
+            <div class="master-time">
+                <h4 class="time-div">04:30 PM</h4>
+                <h5 class="time-div">06:00 PM</h5>
+            </div>
+        </div>
+
+        <div class="col-md-7 col-12">
+            <div class="callout">
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov21-2-trigger">
+                    From Big Screen to Streaming
+                </h4>
+                <p>
+                    This captivating discussion features Manisha Koirala Inconversation with Rana Daggubati. They will
+                    delve into the theory of commercial success as both of them have experienced big screen and
+                    streaming. From their early works to their recent streaming projects, Rana and Koirala will discuss
+                    the challenges and opportunities of adapting their storytelling for different platforms, and how
+                    they continue to captivate audiences with their powerful performances.
+                </p>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-12">
+            <div class="user-profile-icon">
+                <ul>
+                    <li><img src="{{ asset('public/images/master-class/Imtiaz_Ali.avif') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                    <li><img src="{{ asset('public/images/master-class/Kushboo Sundar.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/master-class/nov24.blade.php
+++ resources/views/master-class/nov24.blade.php
@@ -1,37 +1,112 @@
 <div class="mt-4 mb-4">
-    <h2 class="pb-3">November 20th, 2024</h2>
-    {{-- <div class="d-flex tab-content-box"> --}}
+    <h2 class="pb-3">November 24th, 2024</h2>
     <div class="row">
+
         <div class="col-md-2 col-12">
             <div class="master-time">
-                <h4 class="time-div">02:00pm</h4>
-                <h5 class="time-div">05:00pm</h5>
+                <h4 class="time-div">11:00 AM</h4>
+                <h5 class="time-div">12:30 PM</h5>
             </div>
         </div>
+
         <div class="col-md-7 col-12">
             <div class="callout">
-                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab">Beyond The Lens: Funding and
-                    Monetization in Documentary Filmmaking</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s.</p>
-                {{-- <div><a href="#" class="btn cus-prim-btn">Speaker</a></div> --}}
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov24-0-trigger">
+                    Laser for Post Production and Evolution to HDR
+                </h4>
+                <p>
+                    As technology continues to revolutionize the filmmaking process,Anton Muller, Strategic Director at
+                    Barco Cinema,will take us on a journey through the cutting-edge advancements in laser projection and
+                    the evolution to High Dynamic Range (HDR) in this session. He will dig into how these technologies
+                    enhance image quality creating sharper and more immersive cinematic experiences reshaping the future
+                    of post production and the way films are mastered and presented.
+                </p>
             </div>
         </div>
+
         <div class="col-md-3 col-12">
             <div class="user-profile-icon">
                 <ul>
-                    <li><img src="{{ asset('public/images/master-class/svetlana-naudiyal.jpg') }}" alt="image"
-                            class="img-fluid">
-                    </li>
-                    <li><img src="{{ asset('public/images/master-class/svetlana-naudiyal.jpg') }}" alt="image"
-                            class="img-fluid">
-                    </li>
-                    <li><img src="{{ asset('public/images/master-class/svetlana-naudiyal.jpg') }}" alt="image"
+                    <li><img src="{{ asset('public/images/master-class/Imtiaz_Ali.avif') }}" alt="image"
                             class="img-fluid">
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-    {{-- </div> --}}
+</div>
+
+<div class="mt-4 mb-4">
+    <h2 class="pb-3">November 23rd, 2024</h2>
+    <div class="row">
+
+        <div class="col-md-2 col-12">
+            <div class="master-time">
+                <h4 class="time-div">02:30 PM</h4>
+                <h5 class="time-div">04:0 PM</h5>
+            </div>
+        </div>
+
+        <div class="col-md-7 col-12">
+            <div class="callout">
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov24-1-trigger">
+                    Centenary Special: Celebrating the Life and Works of Raj Kapoor - 'The Greatest Showman'
+                </h4>
+                <p>
+                    Raj Kapoor, often hailed as The Greatest Showman of Indian Cinema, left behind an indelible legacy
+                    through his visionary films and larger-than-life persona will be discussed in this special centenary
+                    tribute by his grandson actor Ranbir Kapoor and director, editor Rahul Rawail to celebrate the
+                    legacy of the most influential figures in Indian cinema.This session will reflect on Raj Kapoor's
+                    artistic brilliance, his pioneering contributions to Indian cinema, and the timeless appeal of his
+                    films, which continue to awe filmmakers and audiences alike.
+                </p>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-12">
+            <div class="user-profile-icon">
+                <ul>
+                    <li><img src="{{ asset('public/images/master-class/Imtiaz_Ali.avif') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="mt-4 mb-4">
+    <h2 class="pb-3">November 23rd, 2024</h2>
+    <div class="row">
+
+        <div class="col-md-2 col-12">
+            <div class="master-time">
+                <h4 class="time-div">04:30 PM</h4>
+                <h5 class="time-div">06:00 PM</h5>
+            </div>
+        </div>
+
+        <div class="col-md-7 col-12">
+            <div class="callout">
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov24-2-trigger">
+                    Culture As Context for Cinematic Storytelling
+                </h4>
+                <p>
+                    This session discusses how cultural contexts shape the stories we tell on screen. Renowned writers
+                    will explore how cultural influences, traditions, and values are reflected in films and how they
+                    contribute to the storytelling process.
+                </p>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-12">
+            <div class="user-profile-icon">
+                <ul>
+                    <li><img src="{{ asset('public/images/master-class/Imtiaz_Ali.avif') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
