f83f0c3beab746f08724626a1a54b5ad2d63f37d
aditiya <aditiya@sislinfotech.com>
2024-11-04 18:05:51 +0530

    master class

--- resources/views/master-class/nov27.blade.php
+++ resources/views/master-class/nov27.blade.php
@@ -25,7 +25,10 @@
         <div class="col-md-3 col-12">
             <div class="user-profile-icon">
                 <ul>
-                    <li><img src="{{ asset('public/images/master-class/Imtiaz_Ali.avif') }}" alt="image"
+                    <li><img src="{{ asset('public/images/master-class/Arjun Chakrabarty.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                    <li><img src="{{ asset('public/images/master-class/Manu Chakravarthy.jpg') }}" alt="image"
                             class="img-fluid">
                     </li>
                 </ul>
@@ -33,44 +36,6 @@
         </div>
     </div>
 </div>
-
-{{-- <div class="mt-4 mb-4">
-    <h2 class="pb-3">November 27th, 2024</h2>
-    <div class="row">
-
-        <div class="col-md-2 col-12">
-            <div class="master-time">
-                <h4 class="time-div">12:30 PM</h4>
-                <h5 class="time-div">01:30 PM</h5>
-            </div>
-        </div>
-
-        <div class="col-md-7 col-12">
-            <div class="callout">
-                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov27-1-trigger">
-                    Film Production and it's Stages
-                </h4>
-                <p>
-                    Stephen Woolley will delve into the intricate process of
-                    filmmaking, breaking it down into five essential stages. Stephen Woolley breaks
-                    down the filmmaking process into five key stages: development, pre-production
-                    (including packaging, financing, and casting), production (prep and shooting),
-                    post-production, and release and marketing.
-                </p>
-            </div>
-        </div>
-
-        <div class="col-md-3 col-12">
-            <div class="user-profile-icon">
-                <ul>
-                    <li><img src="{{ asset('public/images/master-class/Imtiaz_Ali.avif') }}" alt="image"
-                            class="img-fluid">
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div> --}}
 
 <div class="mt-4 mb-4">
     <h2 class="pb-3">November 27th, 2024</h2>
@@ -85,7 +50,7 @@
 
         <div class="col-md-7 col-12">
             <div class="callout">
-                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov27-0-trigger">
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov27-1-trigger">
                     Lata Mangeshkar Memorial Talk: Musical Theatre in India
                 </h4>
                 <p>
@@ -111,40 +76,3 @@
         </div>
     </div>
 </div>
-
-{{-- <div class="mt-4 mb-4">
-    <h2 class="pb-3">November 26th, 2024</h2>
-    <div class="row">
-
-        <div class="col-md-2 col-12">
-            <div class="master-time">
-                <h4 class="time-div">04:30 PM</h4>
-                <h5 class="time-div">06:00 PM</h5>
-            </div>
-        </div>
-
-        <div class="col-md-7 col-12">
-            <div class="callout">
-                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov26-3-trigger">
-                    Centenary session : 'Azmaan se aaya farishta' - Mohammad Rafi - The king of
-                    melody
-                </h4>
-                <p>
-                    This is a special Centenary session paying tribute to the king of
-                    melody Mr. Mohammad Rafi. This panel will also have a medley session with Rafi Sab’s
-                    songs.
-                </p>
-            </div>
-        </div>
-
-        <div class="col-md-3 col-12">
-            <div class="user-profile-icon">
-                <ul>
-                    <li><img src="{{ asset('public/images/master-class/Imtiaz_Ali.avif') }}" alt="image"
-                            class="img-fluid">
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div> --}}
