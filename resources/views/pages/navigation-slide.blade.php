ba78bf812c4b496bef6dd138325389c6d22f6f81
aditiya <aditiya@sislinfotech.com>
2024-11-06 19:10:11 +0530

    knowledge series

--- resources/views/pages/navigation-slide.blade.php
+++ resources/views/pages/navigation-slide.blade.php
@@ -449,11 +449,11 @@
                                         <a href="https://filmbazaarindia.com/programme/market-screenings/"
                                             class="nav-link" target="_blank">Market Screenings</a>
                                     </li>
-                                    {{-- <li class="nav-item">
+                                    <li class="nav-item">
                                         <a href="{{ route('knowledge-series') }}" class="nav-link">
-                                            Knowledge series.
+                                            Knowledge series
                                         </a>
-                                    </li> --}}
+                                    </li>
                                 </ul>
                             </div>
                             <!-- Film Bazaar Section End -->
