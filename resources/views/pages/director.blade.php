731479d680adcd979c72b343fb7faf8a83efc949
Avinash <avinashmca94@gmail.com>
2024-11-18 17:54:11 +0530

    changes

--- resources/views/pages/director.blade.php
+++ resources/views/pages/director.blade.php
@@ -3,12 +3,12 @@
     <div class="item card">
         <img src="{{ asset('public/images/cureted-section/' . $director->img_src) }}" alt="image" class="img-fluid">
         <div class="International-competition">
-            <p>{{ $director->slug }}</p>
-            {{-- <p>
+            {{-- <p>{{ $director->slug }}</p> --}}
+            <p>
                 <a href="{{ route('best-director-detail', ['slug' => $director->slug]) }}"
-                    class="post-title">{{ $director->slug }}
+                    class="post-title">{{ $director->title }}
                 </a>
-            </p> --}}
+            </p>
         </div>
     </div>
 @endforeach
