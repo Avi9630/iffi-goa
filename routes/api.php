52e8de2e81eb2ed2d157a5ed65162ac5f7d990c5
Avinash <avinashmca94@gmail.com>
2024-11-20 11:20:23 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -149,5 +149,8 @@ Route::post('csv', [ApiController::class, 'csv'])->name('csv');
 
 //GALLERY
 Route::post('upload-in-galary', [ApiNewController::class, 'uploadInGalary'])->name('upload-in-galary');
-Route::post('gallery',          [GalleryController::class, 'uploadGallery'])->name('gallery');
 Route::post('gallery_upload',   [GalleryController::class, 'upload'])->name('gallery_upload');
+// Route::post('gallery',          [GalleryController::class, 'uploadGallery'])->name('gallery');
+Route::post('all_photo',                    [GalleryController::class, 'allPhoto'])->name('all_photo');
+Route::post('photo_active_inactive/{id}',   [GalleryController::class, 'activeInactive'])->name('photo_active_inactive');
+Route::post('photo_category',               [GalleryController::class, 'photoCategory'])->name('photo_category');
