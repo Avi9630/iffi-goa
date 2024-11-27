7fdeed532b3e669340612784e3ede68919a06904
Avinash <avinashmca94@gmail.com>
2024-11-27 15:58:06 +0530

    changes

--- resources/views/master-class/nov27.blade.php
+++ resources/views/master-class/nov27.blade.php
@@ -72,10 +72,6 @@
                         <img src="{{ asset('public/images/master-class/Himesh Mankad.jpg') }}" alt="image"
                             class="img-fluid">
                     </li>
-                    {{-- <li>
-                        <img src="{{ asset('public/images/master-class/Sajan Raj Kurup.jpg') }}" alt="image"
-                            class="img-fluid">
-                    </li> --}}
                     <li>
                         <img src="{{ asset('public/images/master-class/shankar-ramakrishnan.jpg') }}" alt="image"
                             class="img-fluid">
@@ -144,7 +140,7 @@
                 </h4>
                 <p>
                     From a technological point of view, AI or Artificial Intelligence is a giant leap, but is it going
-                    to benefit  the future of cinema? Or, it can never replace that human touch!... In what ways then AI
+                    to benefit the future of cinema? Or, it can never replace that human touch!... In what ways then AI
                     can be the friend of the entertainment industry?
                     These and many such questions will be answered in this In Conversation session with the CTO of Adani
                     Group Mr. Sudipta Bhattacharya and film producer Mahaveer Jain as they try to unearth the ways of
@@ -159,16 +155,11 @@
                         <img src="{{ asset('public/images/master-class/Sudipta Bhattacharya.jpg') }}" alt="image"
                             class="img-fluid">
                     </li>
-                    {{-- <li>
-                        <img src="{{ asset('public/images/master-class/Vijay Krishna Acharya.png') }}" alt="image"
-                            class="img-fluid">
-                    </li>
-                    <li>
-                        <img src="{{ asset('public/images/master-class/Mahaveer Jain.png') }}" alt="image"
-                            class="img-fluid">
-                    </li> --}}
                 </ul>
             </div>
+            <br>
+            <a href="https://www.youtube.com/watch?v=OEEeT9xtqnk&list=PLUiMfS6qzIMy7gIZtzSgYYLcrVbEc1O7m&index=2"
+                class="btn btn-sm btn-primary" target="_blank" style="margin-left: 80px"> View session</a>
         </div>
     </div>
 </div>
