dc8da696342fa0796070a9acf357facae72d0769
Avinash <avinashmca94@gmail.com>
2025-08-06 16:45:32 +0530

    Changes

--- resources/views/pages/55th-iffi-festival.blade.php
+++ resources/views/pages/55th-iffi-festival.blade.php
@@ -4,7 +4,7 @@
     @endphp
     <div class="col-md-4 col-sm-4 col-xs-12 d-flex">
         <div class="card shadow-sm">
-            <img src="{{ asset('public/images/desktop-image/webp/' . $webpImage) }}" class="img-fluid" alt="image">
+            <img src="{{ asset('public/images/desktop-image/webp/' . $webpImage) }}" class="img-fluid" alt="image" loading="lazy">
             <div class="card-body text-start">
                 <h3>{{ $data->title }}</h3>
                 <p class="card-text">{{ $data->description }}</p>
