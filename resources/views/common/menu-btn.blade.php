4859733999d7de313dac842a158c0fb168ac5cb5
Avinash <avinashmca94@gmail.com>
2025-08-06 17:40:15 +0530

    Changes

--- resources/views/common/menu-btn.blade.php
+++ resources/views/common/menu-btn.blade.php
@@ -1,6 +1,7 @@
 {{-- Menu Btn --}}
 <button class="btn btn-menu" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop"
-    aria-controls="offcanvasTop"> <img src="{{ asset('public/images/menu.svg') }}" alt="image" class="menu-link"
+    aria-controls="offcanvasTop"> 
+    <img src="{{ asset('public/images/menu.svg') }}" alt="image" loading="lazy" class="menu-link"
         data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
     Menu
 </button>
