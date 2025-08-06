a6eee65d8da3fe4b2929cb16340317a0ee122fe2
Avinash <avinashmca94@gmail.com>
2025-08-06 14:58:33 +0530

    Changes

--- resources/views/pages/news-and-update.blade.php
+++ resources/views/pages/news-and-update.blade.php
@@ -12,10 +12,12 @@
                     @endphp
                     <div class="card shadow-sm">
                         <a href="{{ route('news-and-update') }}">
-                            {{-- <img src="{{ asset('public/images/news-update/' . $news->img_src) }}"alt="image"
-                                class="img-fluid"> --}}
-                            <img src="{{ !empty($news->img_src) ? asset('public/images/news-update/webp/' . $webpImage) : $news->image_url }}"
-                                alt="image" class="img-fluid">
+                            <img src="{{ asset('public/images/news-update/webp/' . $webpImage) }}"alt="image"
+                                class="img-fluid">
+                                
+                            {{-- <img src="{{ !empty($news->img_src) ? asset('public/images/news-update/webp/' . $webpImage) : $news->image_url }}"
+                                alt="image" class="img-fluid"> --}}
+
                             <div class="card-body text-start">
                                 <h3>{!! $news->title !!}</h3>
                                 <p class="card-text">
