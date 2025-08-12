a69d392e06c85bf89a8ccb3b3487ac8a4c5cde0b
Avinash <avinashmca94@gmail.com>
2025-08-12 10:20:17 +0530

    Changes

--- app/Http/Controllers/MasterClassController.php
+++ app/Http/Controllers/MasterClassController.php
@@ -12,15 +12,40 @@ class MasterClassController extends Controller
 {
     function index()
     {
-        // $dates = DB::table('master_date')->orderBy('date')->get();
-
-        // return view('master', compact('dates'));
-
-        // Get dates for tabs
         $dates = MasterDate::orderBy('date')->get();
-        // Load all topics with related master_classes, speakers, and moderators
         $topics = MasterClassTopic::with(['masterClass', 'speakers', 'moderators', 'masterDate'])->get();
+        
+        foreach ($dates as $date) {
+            $formattedDateKey = strtolower(date('M', strtotime($date->date))) . date('d', strtotime($date->date));
+            $modalData[$formattedDateKey] = [];
+
+            foreach ($topics->where('master_date_id', $date->id) as $topic) {
+                $modalData[$formattedDateKey][] = [
+                    'title' => $topic->title,
+                    'moderator' => optional($topic->moderators->first())->moderator_name ?? 'TBD',
+                    'panel' => $topic->panel_type,
+                    'date' => date('M jS, Y, h.i A', strtotime($date->date)) . ' TO ' . date('h.i A', strtotime($topic->end_time)),
+                    'speakers' => $topic->speakers
+                        ->map(function ($speaker) {
+                            return [
+                                'name' => $speaker->speaker_name,
+                                'description' => $speaker->speaker_detail,
+                                'image' => asset('public/images/master-class/' . $speaker->image_name),
+                            ];
+                        })
+                        ->toArray(),
+                ];
+            }
+        }
+
+        // dd(json_encode($modalData, true));
+
         // dd($topics);
-        return view('master', compact('dates', 'topics'));
+        // return view('master', compact('dates', 'topics'));
+        return view('master',[
+            'modalData' => json_encode($modalData),
+            'dates' => $dates,
+            'topics' => $topics,
+        ]);
     }
 }
---
+++ resources/views/master-class copy/nov26.blade.php
@@ -0,0 +1,168 @@
+<div class="mt-4 mb-4">
+    <h2 class="pb-3">November 26th, 2024</h2>
+    <div class="row">
+        <div class="col-md-2 col-3">
+            <div class="master-time">
+                <h4 class="time-div">11:00 AM</h4>
+                <h5 class="time-div">12:00 PM</h5>
+            </div>
+        </div>
+        <div class="col-md-7 col-9">
+            <div class="callout">
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov26-0-trigger">
+                    The Thrill of Victory
+                </h4>
+                <p>
+                    Animation Director, Chris Kirshbaum, from DNEG Animation as he delves into the often overlooked
+                    world of sports as a rich reference for understanding human emotion and behaviour in animation.
+                    While animators typically draw inspiration from movies, TV, and real life, sports offer a unique
+                    advantage: the performances are unscripted and authentic, providing invaluable insights into genuine
+                    reactions. Discover how to harness this powerful resource to enhance your animation skills and gain
+                    a fresh perspective on portraying emotion. Don't miss this opportunity to elevate your craft!
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3 col-12">
+            <div class="user-profile-icon">
+                <ul>
+                    <li>
+                        <img src="{{ asset('public/images/master-class/Chris Kirschbaum.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="mt-4 mb-4">
+    <h2 class="pb-3">November 26th, 2024</h2>
+    <div class="row">
+        <div class="col-md-2 col-3">
+            <div class="master-time">
+                <h4 class="time-div">12:30 PM</h4>
+                <h5 class="time-div">01:30 PM</h5>
+            </div>
+        </div>
+        <div class="col-md-7 col-9">
+            <div class="callout">
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov26-1-trigger">
+                    Who is a Film Producer? - Five Important Stages of Film Production
+                </h4>
+                <p>
+                    Stephen Woolley will delve into the intricate process of filmmaking, breaking it down into five
+                    essential stages: development, pre-production (including packaging, financing, and casting),
+                    production (prep and shooting), post-production, and release and marketing.
+                </p>
+            </div>
+        </div>
+        <div class="col-md-3 col-12">
+            <div class="user-profile-icon">
+                <ul>
+                    <li>
+                        <img src="{{ asset('public/images/master-class/Stephen Woolley.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                </ul>
+            </div>
+            <br>
+            <a href="https://www.youtube.com/watch?v=5On4QLR-Jxc&list=PLUiMfS6qzIMy7gIZtzSgYYLcrVbEc1O7m&index=32"
+                class="btn btn-sm btn-primary" target="_blank" style="margin-left: 80px"> View session</a>
+        </div>
+    </div>
+</div>
+
+<div class="mt-4 mb-4">
+    <h2 class="pb-3">November 26th, 2024</h2>
+    <div class="row">
+
+        <div class="col-md-2 col-3">
+            <div class="master-time">
+                <h4 class="time-div">02:30 PM</h4>
+                <h5 class="time-div">04:00 PM</h5>
+            </div>
+        </div>
+
+        <div class="col-md-7 col-9">
+            <div class="callout">
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov26-2-trigger">
+                    Exhibition of Feature Films in Cinema Theaters : The Question of Accessibility
+                </h4>
+                <p>
+                    The accessibility of feature films in cinema theatres remains a
+                    critical issue in the evolving landscape of the different modes of cinema viewing. In this
+                    session, the speaker will address the challenges and solutions surrounding the
+                    exhibition of films in theaters, particularly in making them accessible to wider and more
+                    diverse audiences. From technological advancements to the infrastructure needed for
+                    inclusive viewing, Mr. Senthil Kumar will get into the possibilities of how the industry can
+                    bridge gaps and ensure that the magic of cinema is available to all in cinema hall.
+                </p>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-12">
+            <div class="user-profile-icon">
+                <ul>
+                    <li><img src="{{ asset('public/images/master-class/Senthil Kumar.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                </ul>
+            </div>
+            <br>
+            <a href="https://www.youtube.com/watch?v=SYmXqyqtQ0U&list=PLUiMfS6qzIMy7gIZtzSgYYLcrVbEc1O7m&index=3"
+                class="btn btn-sm btn-primary" target="_blank" style="margin-left: 80px"> View session</a>
+        </div>
+    </div>
+</div>
+
+<div class="mt-4 mb-4">
+    <h2 class="pb-3">November 26th, 2024</h2>
+    <div class="row">
+
+        <div class="col-md-2 col-3">
+            <div class="master-time">
+                <h4 class="time-div">04:30 PM</h4>
+                <h5 class="time-div">06:00 PM</h5>
+            </div>
+        </div>
+
+        <div class="col-md-7 col-9">
+            <div class="callout">
+                <h4 data-bs-toggle="modal" data-bs-target="#exampleModal" class="title-tab" id="nov26-3-trigger">
+                    Centenary session : 'Azmaan se aaya farishta' - Mohammad Rafi - The king of
+                    melody
+                </h4>
+                <p>
+                    Mohammad Rafi, the timeless voice that defined the golden era of Indian film music, continues to
+                    captivate hearts with his melodies. In this centenary session, Sonu Nigam, Shahid Rafi, Subhash Ghai
+                    & Anuradha Paudwal will pay tribute to the legendary 'King of Melody.' Moderated by Yatindra Mishra,
+                    this conversation will explore Rafi's unparalleled contribution to Indian music, the depth of his
+                    emotional expression, and the enduring legacy of his voice that continues to inspire generations of
+                    singers and music lovers worldwide.
+                </p>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-12">
+            <div class="user-profile-icon">
+                <ul>
+                    <li><img src="{{ asset('public/images/master-class/Sonu Nigam.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                    <li><img src="{{ asset('public/images/master-class/Shahid Rafi.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                    <li><img src="{{ asset('public/images/master-class/Subhash Ghai.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                    <li><img src="{{ asset('public/images/master-class/Dr. Anuradha Paudwal.jpg') }}" alt="image"
+                            class="img-fluid">
+                    </li>
+                </ul>
+            </div>
+            <br>
+            <a href="https://www.youtube.com/watch?v=Sf9eZwtx538&list=PLUiMfS6qzIMy7gIZtzSgYYLcrVbEc1O7m&index=2"
+                class="btn btn-sm btn-primary" target="_blank" style="margin-left: 80px"> View session</a>
+        </div>
+    </div>
+</div>
