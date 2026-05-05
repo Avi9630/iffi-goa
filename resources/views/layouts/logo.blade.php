62de2f0d221eb654a34a8e35c74056d40f040b0d
Avi9630 <avinashmca94@gmail.com>
2026-05-05 15:17:46 +0530

    Changes

--- resources/views/layouts/logo.blade.php
+++ resources/views/layouts/logo.blade.php
@@ -24,26 +24,30 @@ if ($newUrl === $currentUrl) {
                 </div>
             </div>
         </div>
-        
+
         <div class="container">
             <div class="top-logo ">
                 <a href="{{ route('/') }}">
-                    <img class="iffi-logo" src="{{ asset('public/images/logo/IFFI56.png') }}" alt="iffi-logo" loading="lazy">
+                    <img class="iffi-logo" src="{{ asset('public/images/logo/IFFI57.png') }}" alt="iffi-logo"
+                        loading="lazy">
                 </a>
                 <a href="https://www.nfdcindia.com/" target="_blank">
                     <img src="{{ asset('public/images/header-logo/nfdc.svg') }}" alt="nfdc-logo" loading="lazy">
                 </a>
                 <a href="https://www.goa.gov.in/" target="_blank">
-                    <img src="{{ asset('public/images/header-logo/goa-govt-logo.png') }}" alt="goa-govt-logo" loading="lazy">
+                    <img src="{{ asset('public/images/header-logo/goa-govt-logo.png') }}" alt="goa-govt-logo"
+                        loading="lazy">
                 </a>
                 <a href="https://www.iffigoa.org">
-                    <img src="{{ asset('public/images/header-logo/iffi-logo.png') }}" alt=" Peacock-fill" class="peacock-logo" loading="lazy">
+                    <img src="{{ asset('public/images/header-logo/iffi-logo.png') }}" alt=" Peacock-fill"
+                        class="peacock-logo" loading="lazy">
                 </a>
                 <a href="https://esg.co.in/" target="_blank">
                     <img src="{{ asset('public/images/header-logo/esg-logo.png') }}" alt="esg-logo" loading="lazy">
                 </a>
                 <a href="https://films.wavesbazaar.com/" target="_blank">
-                    <img src="{{ asset('public/images/header-logo/WavesFilmBazaar.png') }}" alt="Film-Bazar" loading="lazy">
+                    <img src="{{ asset('public/images/header-logo/WavesFilmBazaar.png') }}" alt="Film-Bazar"
+                        loading="lazy">
                 </a>
                 <a href="https://mib.gov.in/ " target="_blank">
                     <img src="{{ asset('public/images/header-logo/mib-logo.svg') }}" alt="mib-logo" loading="lazy">
